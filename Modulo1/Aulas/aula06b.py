43dcd9df7a77a7f85d37ba0e5c5b09fd33a0357c
Raphael <raphaelsfreitas94@gmail.com>
2022-07-04 22:57:20 -0300

    :art: :sparkles: format code and add class 06

---
+++ Modulo1/Aulas/aula06b.py
@@ -0,0 +1,8 @@
+# n = float(input('Digite um valor: '))
+# print(n)
+
+# n = input('Digite algo: ')
+# print(n.isalnum()) #pergunta se é alfanumerico
+
+n = input('Digite algo: ')
+print(n.isupper()) #pergunta se é uppercase
