6a3eaa4e7ca679e4d0f4ab2c71fdfd336278e04b
Raphael <raphaelsfreitas94@gmail.com>
2024-10-13 17:51:32 -0300

    add class 26

---
+++ Exercicios/ex026.py
@@ -0,0 +1,7 @@
+""" Primeira e última ocorrência de uma string """
+
+frase = str(input("Digite uma frase: ")).strip().lower()
+
+print('A letra A aparece {} vezes na frase.'.format(frase.count('a')))
+print('A primeira letra A apareceu na posição {}'.format(frase.find('a') + 1))
+print('A última letra A apareceu na posição {}'.format(frase.rfind('a') + 1))
