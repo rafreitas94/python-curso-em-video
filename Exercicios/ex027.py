408bdf3d0ed1fb7e980c2820cbab1e86ef4620a8
Raphael <raphaelsfreitas94@gmail.com>
2024-10-13 19:00:35 -0300

    add class 27

---
+++ Exercicios/ex027.py
@@ -0,0 +1,9 @@
+""" Primeiro e último nome de uma pessoa """
+
+n = str(input("Digite seu nome completo: ")).strip()
+
+nome = n.split()
+
+print("Muito prazer em conhecer você!")
+print('Seu primeiro nome é {}'.format(nome[0]))
+print('Seu último nome é {}'.format(nome[len(nome) - 1]))
