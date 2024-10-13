3cc1f8e66aa73c61d5539605c0d83688d0bfe959
Raphael <raphaelsfreitas94@gmail.com>
2024-10-13 19:40:46 -0300

    add sleep exercise 28

--- Exercicios/ex028.py
+++ Exercicios/ex028.py
@@ -1,6 +1,7 @@
 """ Jogo da Adivinhação v.1.0 """
 
 from random import randint
+from time import sleep
 
 print('-=-' * 20)
 print('Vou pensar em um número entre 0 e 5. Tente adivinhar...')
@@ -8,6 +9,8 @@ print('-=-' * 20)
 
 computador = randint(0, 5)
 jogador = int(input('Em que número eu pensei? '))
+print('PROCESSANDO...')
+sleep(3)
 
 print(
     'PARABÉNS! Você conseguiu me vencer!' if computador == jogador else
